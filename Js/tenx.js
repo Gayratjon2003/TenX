b7cd244f20c1f6383ceb5b9c3e7f6d31d23bcb0d
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2022-02-06 15:47:04 +0500

    testlar qo'shildi

--- Js/tenx.js
+++ Js/tenx.js
@@ -11,5 +11,17 @@ const list = document.querySelector('.list-blog')
 
 listBtn.addEventListener('click',()=>{
     list.classList.toggle('hidden-list');
+    listBtn.classList.toggle('bx-x')
 
 }) 
+const nextLesson = document.querySelector('.next-lesson');
+const sections = document.querySelectorAll('.section');
+const tests = document.querySelector('.quiz');
+
+nextLesson.addEventListener('click', ()=> {
+    sections.forEach((section)=> {
+        section.style.display = 'none';
+    })
+    list.style.opacity = 0;
+    tests.style.display = 'block';
+})
