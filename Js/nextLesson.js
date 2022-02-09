acdc250370d667d7367eace7fae5a6910cb71792
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2022-02-09 17:49:10 +0500

    api added

--- Js/nextLesson.js
+++ Js/nextLesson.js
@@ -5,12 +5,12 @@ const videoMy = document.querySelector('.video');
 const textBody = document.querySelector('.section-blogs-box-p');
 
 nextLessonMy.addEventListener('click', ()=> {
-    const baseUrl = `http://localhost:1337`;
+    const baseUrl = `https://dry-tundra-80368.herokuapp.com`;
     fetch(`${baseUrl}/api/lessons?populate=*`)
 .then(data=> {
     return data.json()
 }).then(data=> {
-    // console.log(data.data[0].id);
+    // console.log(data.data[0].id);g
         // const element = i;
         // console.log(element);
         // videoMy.src = data.data[0].attributes.video;
@@ -24,6 +24,7 @@ nextLessonMy.addEventListener('click', ()=> {
             textBody.textContent = data.data[1].attributes.content;
         } else if (videoMy.src == data.data[1].attributes.video) {
             console.log('ha');
+            console.log(data.data);
             // console.log(data.data[2].attributes);
             videoMy.src = data.data[2].attributes.video;
 
